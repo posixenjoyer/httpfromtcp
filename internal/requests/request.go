9324d1bbcd8efa186af74d224188afce09c841c8
Jonathan Bowie <j@jtbowie.dev>
2025-05-15 20:30:20 -0400

    update

--- internal/requests/request.go
+++ internal/requests/request.go
@@ -1,6 +1,7 @@
 package request
 
 import (
+	"errors"
 	"fmt"
 	_ "github.com/stretchr/testify/assert"
 	"io"
@@ -8,59 +9,127 @@ import (
 	_ "testing"
 )
 
+func (r RequestLine) IsEmpty() bool {
+	return r == RequestLine{}
+}
+
+func (r *Request) parse(message []byte) (int, error) {
+	var count int
+	var err error
+	var parsedRequestLine RequestLine
+
+	if r.State == DONE {
+		return 0, errors.New("trying to read in DONE state")
+	}
+
+	if r.State == INITIALIZED {
+		parsedRequestLine, count, err = parseRequestLine(message)
+
+		if r.RequestLine.IsEmpty() {
+			r.State = DONE
+			r.RequestLine = parsedRequestLine
+		}
+	} else {
+		return 0, errors.New("unknown state")
+	}
+
+	return count, err
+}
+
+func (r *Request) checkEOFState(buffer []byte, n int) (int, error) {
+	if n > 0 {
+		requestLine, count, err := parseRequestLine(buffer)
+		if requestLine.IsEmpty() {
+			return count, err
+		}
+
+		r.State = DONE
+		r.RequestLine = requestLine
+		return count, nil
+	}
+
+	return 0, errors.New("connection terminated prematurely")
+}
+
 func RequestFromReader(request io.Reader) (*Request, error) {
-	var parsedRequest Request
-
-	lines, err := io.ReadAll(request)
-
-	linesStr := string(lines)
-	if err != nil {
-		fmt.Printf("Request Read failed: %v\n", err)
-	}
-	parsedResult, err := parseRequestLine(strings.Split(linesStr, "\r\n")[0])
-	if err != nil {
-		fmt.Printf("Parsing failed: %v\n", err)
-		parsedRequest.RequestLine = RequestLine{}
-		return &parsedRequest, err
+	lastCount := 0
+	buffer := make([]byte, BUF_SZ)
+	readToIndex := 0
+	parsedRequest := Request{
+		RequestLine: RequestLine{},
+		Body:        make([]byte, 256),
+		Headers:     make(map[string]string),
+		State:       INITIALIZED,
 	}
 
-	parsedRequest.RequestLine = parsedResult
-	parsedRequest.Headers = make(map[string]string)
-	parsedRequest.Body = make([]byte, 8)
+	for parsedRequest.State == INITIALIZED {
+		if readToIndex == BUF_SZ {
+			buffer = append(buffer, make([]byte, readToIndex)...)
+		}
 
+		lastCount, err := request.Read(buffer[readToIndex:])
+		if err != nil {
+			if err == io.EOF {
+				parsedRequest.State = CHECK
+				break
+			}
+		}
+		count, err := parsedRequest.parse(buffer)
+		if err != nil {
+			return &Request{}, err
+		}
+
+		if count > 0 {
+			parsedRequest.State = DONE
+		}
+		readToIndex += lastCount
+	}
+
+	if parsedRequest.State == CHECK {
+		_, err := parsedRequest.checkEOFState(buffer, lastCount)
+		if err != nil {
+			fmt.Printf("EOF read parse failed: %+v\n", err)
+			return &Request{}, err
+		}
+	}
 	return &parsedRequest, nil
 }
 
-func parseRequestLine(requestLine string) (RequestLine, error) {
+func parseRequestLine(requestLine []byte) (RequestLine, int, error) {
 
-	requestArgs := strings.Split(requestLine, " ")
+	requestStr := string(requestLine)
+
+	if !strings.Contains(requestStr, "\r\n") {
+		return RequestLine{}, 0, nil
+	}
+	requestArgs := strings.Split(string(requestLine), " ")
 
 	if len(requestArgs) < 3 {
 		err := fmt.Errorf("error: Not enough fields in request-line")
-		return RequestLine{}, err
+		return RequestLine{}, len(requestStr) + len("\r\n"), err
 	}
 
 	if len(requestArgs) > 3 {
 		err := fmt.Errorf("error: Too many arguments for request-line")
-		return RequestLine{}, err
+		return RequestLine{}, len(requestStr) + len("\r\n"), err
 	}
 
 	fmt.Printf("requestArgs[0]: %s\n", requestArgs[0])
 
 	if !verifyMethod(requestArgs[0]) {
 		err := fmt.Errorf("error: Method name verification failed")
-		return RequestLine{}, err
+		return RequestLine{}, len(requestStr) + len("\r\n"), err
 	}
 
 	if !verifyVersion(requestArgs[2]) {
 		err := fmt.Errorf("error: Invalid HTTP Version: %s", requestArgs[2])
-		return RequestLine{}, err
+		return RequestLine{}, len(requestStr) + len("\r\n"), err
 	}
 	parsedReqestLine := RequestLine{
-		Method:      requestArgs[0],
-		Target:      requestArgs[1],
-		HttpVersion: strings.Split(requestArgs[2], "/")[1],
+		Method:        requestArgs[0],
+		RequestTarget: requestArgs[1],
+		HttpVersion:   strings.Split(requestArgs[2], "/")[1],
 	}
 
-	return parsedReqestLine, nil
+	return parsedReqestLine, len(requestStr) + len("\r\n"), nil
 }
--- internal/requests/types.go
+++ internal/requests/types.go
@@ -1,15 +1,21 @@
 package request
 
+const DONE int = 2
+const INITIALIZED int = 1
+const CHECK int = 3
+const BUF_SZ int = 8
+
 type Request struct {
 	RequestLine RequestLine
 	Headers     map[string]string
 	Body        []byte
+	State       int
 }
 
 type RequestLine struct {
-	Method      string
-	Target      string
-	HttpVersion string
+	Method        string
+	RequestTarget string
+	HttpVersion   string
 }
 
 var httpMethods []string = []string{
